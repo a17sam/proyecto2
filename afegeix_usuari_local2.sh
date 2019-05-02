2f0af71af1965338ad66d9095a19e581b06d3009
Tholamlam <50111720+Tholamlam@users.noreply.github.com>
2019-05-02 20:10:25 +0200

    Update afegeix_usuari_local2.sh

--- afegeix_usuari_local2.sh
+++ afegeix_usuari_local2.sh
@@ -14,3 +14,25 @@ if [ $(id -u) -eq 0 ]; then
 	echo "Uso del programa: $0 usuario nombre completo"
 	exit 1
 	fi
+  
+# The first parameter is the user name.
+	username=$1
+	# The rest of the parameters are for the account comments.
+	fulln=${@:2}
+	# Generate a password.
+	password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 13 | head -n 1)
+	pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+	fullnospaces="$(echo -e "${fulln}" | tr -d '[:space:]')"
+
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username ya existe!"
+		exit 2
+	else
+		# Create the user with the password.
+		useradd -m -c $fullnospaces -p $pass $username
+		hostname=`hostname`
+		# Check to see if the useradd command succeeded.
+		# Set the password.
+		# Check to see if the passwd command succeeded.
+		# Display the username, password, and the host where the user was created.
